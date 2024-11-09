1145a79c1ae8fef7f189a6c12747651bc6fda3d0
Jamil <jamilahmediiuc@gmail.com>
2024-11-09 11:31:56 +0600

    Update visualization.py

--- misc/visualization.py
+++ misc/visualization.py
@@ -87,7 +87,7 @@ def joints_dict():
                 [1, 7], [2, 8], [1, 2], [1, 3], [2, 4], 
                 [3, 5], [4, 6], [0, 1], [0, 2], [0, 13]  # Add edge from nose to xyz
             ]
-        },
+        }
     }
     return joints
 
